80ff4374bd8b5b570d9079f6dedd9d3d6895d7dd
Aziza Kadyrova <azziza.kadyrova@gmail.com>
2022-06-17 11:50:07 +0600

    pervyi

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.mySlider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        dots:true,
+        arrow:true,
+        pauseOnHover:true,
+        fade:true
+    });
+  });
